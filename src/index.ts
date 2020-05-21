1379f905d7bd13440d1ade228ee3f44103879805
Romain Bourjot <romain.bourjot@gmail.com>
2020-05-21 14:55:35 +0200

    feat(core): add simple route handling

---
+++ src/index.ts
@@ -0,0 +1,53 @@
+interface Result<Context> {
+  method: string;
+  matchedPath: string;
+  path: string;
+  params: Map<string, string>;
+  context: Context;
+}
+
+export interface Definition<Context> {
+  method: string;
+  path: string;
+  context: Context;
+}
+
+type Find<Context> = (method: string, path: string) => Result<Context> | null;
+
+export interface Router<Context> {
+  find: Find<Context>;
+}
+
+function formatIndexer(method: string, path: string): string {
+  return `${method}|${path}`;
+}
+
+export function createRouter<Context>(definitions: Definition<Context>[]): Router<Context> {
+  const indexedMap = new Map<string, Definition<Context>>();
+
+  for (let i = 0; i < definitions.length; i++) {
+    const definition = definitions[i];
+    const indexer = formatIndexer(definition.method, definition.path);
+
+    indexedMap.set(indexer, definition);
+  }
+
+  return {
+    find: (method: string, path: string): Result<Context | null> => {
+      const indexer = formatIndexer(method, path);
+      const found = indexedMap.get(indexer);
+
+      if (found) {
+        return {
+          method,
+          matchedPath: found.path,
+          path,
+          params: new Map(),
+          context: found.context,
+        };
+      }
+
+      return null;
+    },
+  };
+}
---
+++ src/tests/functional/simple-usage.ts
@@ -0,0 +1,91 @@
+import test from 'tape';
+
+import { createRouter } from '@src/index';
+
+const getSlashSymbol = Symbol('get_slash');
+const postSlashSymbol = Symbol('post_slash');
+const getHelloSymbol = Symbol('get_hello');
+
+const getDefinitions = () => [
+  {
+    method: 'GET',
+    path: '/',
+    context: getSlashSymbol,
+  },
+  {
+    method: 'POST',
+    path: '/',
+    context: postSlashSymbol,
+  },
+  {
+    method: 'GET',
+    path: '/hello',
+    context: getHelloSymbol,
+  },
+];
+
+test('CAT0_functional/simple-usage | GET slash', (t) => {
+  t.plan(1);
+
+  const router = createRouter(getDefinitions());
+
+  const actual = router.find('GET', '/');
+
+  const expected = {
+    method: 'GET',
+    matchedPath: '/',
+    path: '/',
+    params: new Map(),
+    context: getSlashSymbol,
+  };
+
+  t.deepEqual(actual, expected);
+});
+
+test('CAT0_functional/simple-usage | POST slash', (t) => {
+  t.plan(1);
+
+  const router = createRouter(getDefinitions());
+
+  const actual = router.find('POST', '/');
+
+  const expected = {
+    method: 'POST',
+    matchedPath: '/',
+    path: '/',
+    params: new Map(),
+    context: postSlashSymbol,
+  };
+
+  t.deepEqual(actual, expected);
+});
+
+test('CAT0_functional/simple-usage | GET hello', (t) => {
+  t.plan(1);
+
+  const router = createRouter(getDefinitions());
+
+  const actual = router.find('GET', '/hello');
+
+  const expected = {
+    method: 'GET',
+    matchedPath: '/hello',
+    path: '/hello',
+    params: new Map(),
+    context: getHelloSymbol,
+  };
+
+  t.deepEqual(actual, expected);
+});
+
+test('CAT1_functional/simple-usage | GET unknown', (t) => {
+  t.plan(1);
+
+  const router = createRouter(getDefinitions());
+
+  const actual = router.find('GET', '/unknown');
+
+  const expected = null;
+
+  t.deepEqual(actual, expected);
+});
