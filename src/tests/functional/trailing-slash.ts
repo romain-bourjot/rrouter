c4de5f9cfeba383ebdbbf003b3181c79488390fc
Romain Bourjot <romain.bourjot@gmail.com>
2020-05-22 18:08:35 +0200

    feat(core): handle trailing slash

---
+++ src/tests/functional/trailing-slash.ts
@@ -0,0 +1,81 @@
+import test from 'tape';
+
+import { createRouter } from '@src/index';
+
+test('CAT0_functional/trailing-slash | Trailing slash in request', (t) => {
+  t.plan(1);
+
+  const symbol = Symbol('expected');
+  const definitions = [
+    {
+      method: 'GET',
+      path: '/foo/bar',
+      context: symbol,
+    },
+  ];
+  const router = createRouter(definitions);
+
+  const actual = router.find('GET', '/foo/bar/');
+
+  const expected = {
+    method: 'GET',
+    matchedPath: '/foo/bar',
+    path: '/foo/bar',
+    params: new Map(),
+    context: symbol,
+  };
+
+  t.deepEqual(actual, expected);
+});
+
+test('CAT0_functional/trailing-slash | Trailing slash in definition', (t) => {
+  t.plan(1);
+
+  const symbol = Symbol('expected');
+  const definitions = [
+    {
+      method: 'GET',
+      path: '/foo/bar/',
+      context: symbol,
+    },
+  ];
+  const router = createRouter(definitions);
+
+  const actual = router.find('GET', '/foo/bar');
+
+  const expected = {
+    method: 'GET',
+    matchedPath: '/foo/bar',
+    path: '/foo/bar',
+    params: new Map(),
+    context: symbol,
+  };
+
+  t.deepEqual(actual, expected);
+});
+
+test('CAT0_functional/trailing-slash | Root', (t) => {
+  t.plan(1);
+
+  const symbol = Symbol('expected');
+  const definitions = [
+    {
+      method: 'GET',
+      path: '/',
+      context: symbol,
+    },
+  ];
+  const router = createRouter(definitions);
+
+  const actual = router.find('GET', '/');
+
+  const expected = {
+    method: 'GET',
+    matchedPath: '/',
+    path: '/',
+    params: new Map(),
+    context: symbol,
+  };
+
+  t.deepEqual(actual, expected);
+});
