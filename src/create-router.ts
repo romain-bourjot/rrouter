9ca34519c5926916872db41591f9ee64e41ed1d6
Romain Bourjot <romain.bourjot@gmail.com>
2020-05-23 02:57:04 +0200

    feat(core): handle parametrized paths

---
+++ src/create-router.ts
@@ -0,0 +1,57 @@
+import { Definition, Router, Result } from './types';
+import {
+  removeTrailingSlash,
+  isPathLengthValid,
+  isMethodSupported,
+} from './utils';
+
+import { createRouter as createMapRouter } from './impls/map';
+import { createRouter as createTreeRouter } from './impls/tree';
+
+export function createRouter<Context>(definitions: Definition<Context>[]): Router<Context> {
+  const mapDefinitions: Definition<Context>[] = [];
+  const treeDefinitions: Definition<Context>[] = [];
+
+  for (let i = 0; i < definitions.length; i++) {
+    const definition = definitions[i];
+    definition.path = removeTrailingSlash(definition.path);
+
+    if (!isPathLengthValid(definition.path)) {
+      throw new Error(`Invalid definition, path is too long: ${definition.method} ${definition.path}`);
+    }
+
+    if (!isMethodSupported(definition.method)) {
+      throw new Error(`Invalid definition, unsupported method: ${definition.method} ${definition.path}`);
+    }
+
+    if (definition.path.includes('/:')) {
+      treeDefinitions.push(definition);
+    } else {
+      mapDefinitions.push(definition);
+    }
+  }
+
+  const mapRouter = createMapRouter(mapDefinitions);
+  const treeRouter = createTreeRouter(treeDefinitions);
+
+  return {
+    find: (method: string, rawPath: string): Result<Context | null> => {
+      if (!isMethodSupported(method)) {
+        return null;
+      }
+
+      if (!isPathLengthValid(rawPath)) {
+        return null;
+      }
+
+      const path = removeTrailingSlash(rawPath);
+
+      const mapFound = mapRouter.find(method, path);
+      if (mapFound) {
+        return mapFound;
+      }
+
+      return treeRouter.find(method, path);
+    },
+  };
+}
---
+++ src/types.ts
@@ -0,0 +1,19 @@
+export interface Definition<Context> {
+  method: string;
+  path: string;
+  context: Context;
+}
+
+type Find<Context> = (method: string, path: string) => Result<Context> | null;
+
+export interface Router<Context> {
+  find: Find<Context>;
+}
+
+export interface Result<Context> {
+  method: string;
+  matchedPath: string;
+  path: string;
+  params: Map<string, string>;
+  context: Context;
+}
