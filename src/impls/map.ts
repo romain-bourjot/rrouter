10c02c3e6c3c4f12d9edce21d37e9649dffbcd11
Romain Bourjot <romain.bourjot@gmail.com>
2020-05-23 03:00:05 +0200

    perf(map-router): improve map-router with path length shortcut

--- src/impls/map.ts
+++ src/impls/map.ts
@@ -6,9 +6,11 @@ function formatIndexer(method: string, path: string): string {
 
 export function createRouter<Context>(definitions: Definition<Context>[]): Router<Context> {
   const indexedMap = new Map<string, Definition<Context>>();
+  const lengths = new Set();
 
   for (let i = 0; i < definitions.length; i++) {
     const definition = definitions[i];
+    lengths.add(definition.path.length);
 
     const indexer = formatIndexer(definition.method, definition.path);
 
@@ -17,6 +19,10 @@ export function createRouter<Context>(definitions: Definition<Context>[]): Route
 
   return {
     find: (method: string, path: string): Result<Context | null> => {
+      if (!lengths.has(path.length)) {
+        return null;
+      }
+
       const indexer = formatIndexer(method, path);
       const found = indexedMap.get(indexer);
 
